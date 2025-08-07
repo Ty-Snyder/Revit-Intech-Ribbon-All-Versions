fcd21813a1ee06f1ff63c03eea98038567126360
tsnyder <tyrysny@gmail.com>
2025-08-07 15:15:27 -0700

    Added 2026 support

---
+++ Revit 2026/App.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.UI;
+using SharedRevit.Events;
+using SharedRevit.Utils;
+using SharedCore;
+
+namespace Revit_2026
+{
+    public class App : IExternalApplication
+    {
+        internal string basePath;
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string dllPath = typeof(App).Assembly.Location;
+            basePath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(dllPath)));
+            SharedCore.App.BasePath = basePath;
+            SaveFileEventLink.Register(application);
+
+            new Ribbon(application , basePath);
+
+            SharedRevit.Utils.RevitUtilService.Initialize(new RevitUtils());
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            SaveFileEventLink.Cleanup();
+            return Result.Succeeded;
+        }
+    }
+}
+
---
+++ Revit 2026/Ribbon.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.UI;
+using SharedRevit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_2026
+{
+    internal class Ribbon : SharedRevit.Ribbon.DefaultRibbonBuild
+    {
+        public Ribbon(UIControlledApplication app, string basePath) : 
+            base(app, basePath, "SharedRevit",Assembly.GetExecutingAssembly().Location)
+        {
+            // Initialize the ribbon with the application and base path
+        }
+    }
+}
